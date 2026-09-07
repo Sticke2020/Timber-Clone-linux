9aa68dad8689a261222cbd2c395dbd3b01209ab7
sticke2020 <sticke2020@gmail.com>
2026-09-07 14:01:47 -0500

    Initial commit

---
+++ build/CMakeFiles/Timber.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Timber.dir/link.d"
+  "CMakeFiles/Timber.dir/src/Timber.cpp.o"
+  "CMakeFiles/Timber.dir/src/Timber.cpp.o.d"
+  "Timber"
+  "Timber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Timber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Timber.cpp
@@ -0,0 +1,33 @@
+
+// Include important libraries here
+#include <SFML/Graphics.hpp>
+
+// Make the code easier to type with "using namespace" using namespace sf;
+
+// This is where our game starts from int main()
+int main() {
+   // Create a video mode object VideoMode vm(1920, 1080);
+
+   // Create and open a window for the game
+   RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+   while (window.isOpen()) {
+
+    /*Handle the players input*/
+    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+        window.close();
+    }
+
+    /*Update the scene*/
+
+    /*Draw the scene*/
+
+    // Clear everything from the last frame window.clear();
+
+    // Draw our game scene here
+
+    // Show everything we just drew window.display();
+   }
+
+    return 0;
+}
